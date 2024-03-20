703e3f2307f373ff58796be86631f814df1efe19
Teacher <teacher-23@ah4tjp1ldq6w2.78b420.nycboe.org>
2024-03-20 14:14:34 -0400

    New changes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Nav Java
+let searchBar= document.querySelector(".searchBar");
+// onmouseover & onmouseout
 
+searchBar.addEventListener("focus", inputFocus);
+
+function inputFocus(){
+document.querySelector(".midNav").style.border="2px solid red";
+}
+
+searchBar.addEventListener("focusout", inputUnfocus);
+function inputUnfocus(){
+    document.querySelector(".midNav").style.border="none";
+    }
+
+    // window.onload= function loginForm(){
+    //     document.querySelector("body").style.backgroundColor="red"
+    
+    //     document.querySelector(".Login-form").style.display="block"
+    // }
